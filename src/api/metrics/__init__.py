1bc3f5b126f3107abfb9422e43c1240491bf7133
aliyor-azizovich <126095498+aliyor-azizovich@users.noreply.github.com>
2025-10-22 13:08:26 +0500

    Create __init__.py

---
+++ src/api/metrics/__init__.py
@@ -0,0 +1,11 @@
+from .iaao import iaao_metrics, full_report, ratios, mape, mdape
+from .regression_diagnostics import trend_on_ratio
+
+__all__ = [
+    "iaao_metrics",
+    "full_report",
+    "ratios",
+    "mape",
+    "mdape",
+    "trend_on_ratio",
+]
